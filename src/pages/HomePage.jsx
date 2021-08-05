e4062f1ff4e8f15fdb21476cdb5e1bd286b24b93
Your Namechristophe-cpu <christophe-michaud1@orange.fr>
2021-08-05 20:25:05 +0200

    homepage modified

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -10,7 +10,7 @@ function HomePage() {
                     <img  className="img-home" src={imagecentrale} alt="porte d'entrée" />
                 </div>
                 <div className="encart encart1">
-                    <p className="desc">Bienvenue devant l'Antre Divin
+                    <p className="desc">Bienvenue devant l'Antre Divin.
                     Après une journée de visite dans l'Olympe, vous voilà
                     bien fatigué ! 
                     Rien de mieux qu'une taverne pour boire un coup et 
@@ -20,7 +20,7 @@ function HomePage() {
                     <p className="desc">Mais attention, beaucoup de choses 
                     peuvent se passer derrière cette porte, vous êtes tout 
                     de même sur le territoire des dieux et déesses de l'Olympe
-                Restez sur vos gardes ...</p>
+                    Restez sur vos gardes ...</p>
                 </div>
                 <div className="fronton">
                     L'ANTRE du DIVIN
