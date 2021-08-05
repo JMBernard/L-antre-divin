1a8cd6918be73bb3cbcc6ac56441fe262926d4f6
JMBernard <jm11bernard@gmail.com>
2021-08-05 16:05:26 +0200

    background tavern ok

---
+++ src/pages/Tavern.jsx
@@ -0,0 +1,13 @@
+import tavern from '../assets/tavern.png'
+
+import './Tavern.css'
+
+function Tavern() {
+    return (
+        <div className='Tavern'>
+            <img className='background-tavern' src={tavern} alt=""/>
+        </div>
+    )
+}
+
+export default Tavern;
